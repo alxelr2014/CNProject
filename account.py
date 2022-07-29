e76494b4973fba66a5fa34c6dbbb5a7c60160c15
Ali <a80.abbasi@yahoo.com>
2022-07-29 22:58:31 +0430

    Unified user classes

--- account.py
+++ account.py
@@ -1,7 +1,23 @@
-
-
 class Account:
-    def __init__(self,username,password):
-        self.username = username;
+    def __init__(self, username, password):
+        self.username = username
         self.password = password
-        
+
+
+class User(Account):
+    def __init__(self, username, password):
+        super().__init__(username, password)
+
+
+class Admin(Account):
+    def __init__(self, username, password):
+        super().__init__(username, password)
+        self.username = username
+        self.password = password
+
+
+class Manager(Account):
+    def __init__(self, username, password):
+        super().__init__(username, password)
+        self.username = username
+        self.password = password
