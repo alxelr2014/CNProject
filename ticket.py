e91c1e050401b95e3e18e6679bbe6664dc6c2fd7
MJavadHzr <hezarej@gmail.com>
2022-08-06 23:57:15 +0430

    ticket model

--- ticket.py
+++ ticket.py
@@ -9,7 +9,18 @@ class TicketState(enum.Enum):
 
 
 class Ticket:
-    def __init__(self, owner, state, content):
+    def __init__(self, id, owner, content):
+        self.id = id
         self.owner = owner
-        self.state = state
-        self.content = content
+        self.state = TicketState.NEW
+        self.content = [(owner, content)]
+
+    def add_message(self, username, message):
+        self.content.append((username, message))
+
+
+def find_ticket(ticket_id, all_tickets):
+    for ticket in all_tickets:
+        if ticket.id == ticket_id:
+            return ticket
+    return None
